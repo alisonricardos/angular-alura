04254be1b4a009a2de7ca927f7b627cce7655fb4
alisonricardos <alisonricardos@users.noreply.github.com>
2019-03-14 13:07:29 -0300

    First Commit!

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,37 @@
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+
+    // $scope.fotos = [
+    //    {
+    //        titulo: 'Leão',
+    //        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    //    },
+    //    {
+    //        titulo: 'Leao 02',
+    //        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    //    },
+    //    {
+    //        titulo: 'Leão',
+    //        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    //    }
+    //];
+
+    $scope.fotos = [];
+
+    $http.get('v1/fotos')
+        .success(function (retorno) {
+            console.log(retorno);
+            $scope.fotos = retorno;
+        })
+        .error(function (erro) {
+            console.log(erro);
+        });
+    ////////////////Esse método faz a mesma coisa do acima
+    //var promise = $http.get('v1/fotos');// retorna uma promessa das fotos da url v1/fotos, 
+    //promise.then(function (retorno) {
+    //    $scope.fotos = retorno.data;
+    //}).catch(function (error) {
+    //    console.log(error)
+    //    });
+
+
+});
